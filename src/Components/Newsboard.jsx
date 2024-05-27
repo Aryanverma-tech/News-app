9cd53e78f5c12a674bee0f4ce0bbf6cde306d04d
Aryan <aryanverma51102@gmail.com>
2024-05-28 03:41:45 +0530

    news-app

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -8,7 +8,7 @@ const Newsboard = ({category}) => {
     const [articles,setArticles]=useState([]);
 
     useEffect(()=>{
-      let url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=1bd6def437d34b31b1cd58f246f51295`;
+      let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=1bd6def437d34b31b1cd58f246f51295`;
       // let url=`https://newsapi.org/v2/everything?q=tesla&from=2024-04-27&sortBy=publishedAt&apiKey=1bd6def437d34b31b1cd58f246f51295`;
         // let url=` https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
